ef3b073cd3b2a0357e4a6d04cddb181d614e959f
YANKIT CHAUHAN <yankit.chauhan@nagarro.com>
2026-04-15 09:58:15 +0530

    trigger deployment

--- src/main/java/com/shopsy/product/ProductServiceApplication.java
+++ src/main/java/com/shopsy/product/ProductServiceApplication.java
@@ -3,6 +3,9 @@ package com.shopsy.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Trigger Deployment.
+ */
 @SpringBootApplication
 public class ProductServiceApplication {
     public static void main(String[] args) {
